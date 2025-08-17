c06537adb5dbc1d3642bcd200782fd1e1f535a1e
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-08-16 21:41:24 -0300

    Criando Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Card from "./componentes/Card/Card";
 import Estilos from "./componentes/EstilosGlobais/Estilos";
 
 function App() {
   return (
     <div className="App">
       <Estilos />
+      <Card>
         <h1>Freelando</h1>
+      </Card>
     </div>
   );
 }
---
+++ src/componentes/Card/Card.jsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const DivEstilizada = styled.div`
+  padding: 32px;
+  background: #ebeaf9;
+  border: 1px solid #5754ed;
+  border-radius: 16px;
+`;
+
+const Card = ({ children }) => {
+  return <DivEstilizada>{children}</DivEstilizada>;
+};
+
+export default Card;
