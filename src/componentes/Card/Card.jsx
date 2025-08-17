a791b6bb7b658efa49f88f3051c517e95455663c
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-08-17 14:33:41 -0300

    Consumindo os espaçamentos

--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -1,11 +1,11 @@
 import styled from "@emotion/styled";
 
 const DivEstilizada = styled.div`
-  padding: 32px;
+  padding: ${(props) => props.theme.espacamentos.l};
   background: ${(props) => props.theme.cores.secundarias.a};
   border: 1px solid;
   border-color: ${(props) => props.theme.cores.primarias.a};
-  border-radius: 16px;
+  border-radius: ${(props) => props.theme.espacamentos.s};
 `;
 
 const Card = ({ children }) => {
