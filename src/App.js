1071e551dca873c15b5272b23476f14496a8a25a
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-08-22 14:43:47 -0300

    Container react-grid-system

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Container } from "react-grid-system";
 import Cabecalho from "./componentes/Cabecalho/Cabecelho";
 import CampoTexto from "./componentes/CampoTexto/CampoTexto";
 import Card from "./componentes/Card/Card";
@@ -11,20 +12,22 @@ const App = () => {
     <ProvedorTema>
       <Estilos />
       <Cabecalho />
-      <Card>
-        <Tipografia variante="h1" componente="h1">
-          Freelando
-        </Tipografia>
-        <Tipografia variante="body" componente="body">
-          Crie seu perfil gratuitamente para começar a trabalhar com os melhores
-          freelancers. Em seguida, você poderá dar mais detalhes sobre suas
-          demandas e sobre sua forma de trabalho.
-        </Tipografia>
-        <CampoTexto titulo="Nome Completo"/>
-      </Card>
+      <Container>
+        <Card>
+          <Tipografia variante="h1" componente="h1">
+            Freelando
+          </Tipografia>
+          <Tipografia variante="body" componente="body">
+            Crie seu perfil gratuitamente para começar a trabalhar com os
+            melhores freelancers. Em seguida, você poderá dar mais detalhes
+            sobre suas demandas e sobre sua forma de trabalho.
+          </Tipografia>
+          <CampoTexto titulo="Nome Completo" />
+        </Card>
+      </Container>
       <Rodape />
     </ProvedorTema>
   );
-}
+};
 
 export default App;
