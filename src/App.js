f89d1e23cb0a4b6b99ab9584e0cc066d2d11804c
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-08-23 18:44:30 -0300

    Adicionando componente ListaSuspensa

--- src/App.js
+++ src/App.js
@@ -17,6 +17,36 @@ import { IconeTwitter } from "./componentes/Icones/IconeTwitter";
 import { IconeWhatsApp } from "./componentes/Icones/IconeWhatsApp";
 import ListaSuspensa from "./componentes/ListaSuspensa/ListaSuspensa";
 
+const estadosBrasileiros = [
+  { "text": "Acre", "value": "AC" },
+  { "text": "Alagoas", "value": "AL" },
+  { "text": "Amapá", "value": "AP" },
+  { "text": "Amazonas", "value": "AM" },
+  { "text": "Bahia", "value": "BA" },
+  { "text": "Ceará", "value": "CE" },
+  { "text": "Distrito Federal", "value": "DF" },
+  { "text": "Espírito Santo", "value": "ES" },
+  { "text": "Goiás", "value": "GO" },
+  { "text": "Maranhão", "value": "MA" },
+  { "text": "Mato Grosso", "value": "MT" },
+  { "text": "Mato Grosso do Sul", "value": "MS" },
+  { "text": "Minas Gerais", "value": "MG" },
+  { "text": "Pará", "value": "PA" },
+  { "text": "Paraíba", "value": "PB" },
+  { "text": "Paraná", "value": "PR" },
+  { "text": "Pernambuco", "value": "PE" },
+  { "text": "Piauí", "value": "PI" },
+  { "text": "Rio de Janeiro", "value": "RJ" },
+  { "text": "Rio Grande do Norte", "value": "RN" },
+  { "text": "Rio Grande do Sul", "value": "RS" },
+  { "text": "Rondônia", "value": "RO" },
+  { "text": "Roraima", "value": "RR" },
+  { "text": "Santa Catarina", "value": "SC" },
+  { "text": "São Paulo", "value": "SP" },
+  { "text": "Sergipe", "value": "SE" },
+  { "text": "Tocantins", "value": "TO" }
+]
+
 const App = () => {
   return (
     <ProvedorTema>
@@ -52,7 +82,7 @@ const App = () => {
               </Row>
               <Row>
                 <Col lg={4} md={4} sm={4}>
-                  <ListaSuspensa titulo="Estado" />
+                  <ListaSuspensa titulo="Estado" opcoes={estadosBrasileiros}/>
                 </Col>
                 <Col lg={8} md={8} sm={8}>
                   <CampoTexto titulo="Cidade" />
--- src/componentes/ListaSuspensa/ListaSuspensa.jsx
+++ src/componentes/ListaSuspensa/ListaSuspensa.jsx
@@ -1,58 +1,135 @@
-import styled from "@emotion/styled"
-import { useState } from "react"
+import styled from "@emotion/styled";
+import { useState } from "react";
 
 const LabelEstilizada = styled.label`
-    display: block;
-    width: 100%;
-    box-sizing: border-box;
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 24px;
-    position: relative;
-
-`
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  position: relative;
+`;
 
 const BotaoEstilizado = styled.button`
-    cursor: pointer;
-    display: block;
-    height: 40px;
-    width: 100%;
-    font-size: 18px;
-    outline: none;
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-weight: 400;
-    border-radius: 18px;
-    border-bottom-left-radius: ${props => props.estaAberta ? '0' : '18px'};
-    border-bottom-right-radius: ${props => props.estaAberta ? '0' : '18px'};
-    margin-top: ${props => props.theme.espacamentos.xs};
-    padding: ${props => props.theme.espacamentos.s};
-    background: ${props => props.theme.cores.branco};
-    border: 1px solid ${props => props.theme.cores.neutras.a};
-    &:focus {
-        border-color: ${props => props.theme.cores.focus};
-    }
-`
+  cursor: pointer;
+  display: block;
+  height: 40px;
+  width: 100%;
+  font-size: 18px;
+  outline: none;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-weight: 400;
+  border-radius: 18px;
+  border-bottom-left-radius: ${(props) => (props.estaAberta ? "0" : "18px")};
+  border-bottom-right-radius: ${(props) => (props.estaAberta ? "0" : "18px")};
+  margin-top: ${(props) => props.theme.espacamentos.xs};
+  padding: ${(props) => props.theme.espacamentos.s};
+  background: ${(props) => props.theme.cores.branco};
+  border: 1px solid ${(props) => props.theme.cores.neutras.a};
+  &:focus {
+    border-color: ${(props) => props.theme.cores.focus};
+  }
+`;
+
+const ListaSuspensa = styled.ul`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  background-color: ${(props) => props.theme.cores.branco};
+  z-index: 1;
+  border: 1px solid ${(props) => props.theme.cores.neutras.a};
+  border-bottom-left-radius: 18px;
+  border-bottom-right-radius: 18px;
+  border-top: none;
+  margin: 0;
+  padding: 0 ${(props) => props.theme.espacamentos.m};
+  list-style: none;
+`;
+
+const ItemLista = styled.li`
+  padding: ${(props) => props.theme.espacamentos.xs} 0;
+  text-align: center;
+  border-bottom: 1px solid ${(props) => props.theme.cores.neutras.c};
+  cursor: pointer;
+  &:last-child {
+    border: none;
+  }
+  color: ${(props) => (props.focoAtivo ? props.theme.cores.focus : "inherit")};
+  &:hover {
+    color: ${(props) => props.theme.cores.focus};
+  }
+`;
 
 const ListaSupensa = ({ titulo, opcoes }) => {
-    const [estaAberta, alternarVisibilidade] = useState(false)
+  const [estaAberta, alternarVisibilidade] = useState(false);
+  const [opcaoFocada, setOpcaoFocada] = useState(null);
+  const [opcaoSelecionada, setOpcaoSelecionada] = useState(null);
 
-    return (<LabelEstilizada>
-        {titulo}
-        <BotaoEstilizado
-             estaAberta={estaAberta}
-             onClick={() => alternarVisibilidade(!estaAberta)}
-        >
-            <div>
-                Selecione
-            </div>
-            <div>
-                <span>{estaAberta ? '▲' : '▼'}</span>
-            </div>
-        </BotaoEstilizado>
-    </LabelEstilizada>)
-}
+  const manipularTeclaDoTeclado = (evento) => {
+    alternarVisibilidade(true);
+    switch (evento.key) {
+      case "ArrowDown":
+        evento.preventDefault();
+        setOpcaoFocada((focoAntigo) => {
+          if (focoAntigo === null) {
+            return 0;
+          }
+          return (focoAntigo += 1);
+        });
+        break;
+      case "ArrowUp":
+        evento.preventDefault();
+        setOpcaoFocada((focoAntigo) => {
+          if (!focoAntigo) {
+            return 0;
+          }
+          return (focoAntigo -= 1);
+        });
+        break;
+      case "Enter":
+        evento.preventDefault();
+        setOpcaoFocada(null);
+        alternarVisibilidade(false);
+        setOpcaoSelecionada(opcoes[opcaoFocada]);
+        break;
+      default:
+        break;
+    }
+  };
 
-export default ListaSupensa
+  return (
+    <LabelEstilizada>
+      {titulo}
+      <BotaoEstilizado
+        estaAberta={estaAberta}
+        onClick={() => alternarVisibilidade(!estaAberta)}
+        onKeyDown={manipularTeclaDoTeclado}
+      >
+        <div>{opcaoSelecionada ? opcaoSelecionada.text : "Selecione"}</div>
+        <div>
+          <span>{estaAberta ? "▲" : "▼"}</span>
+        </div>
+      </BotaoEstilizado>
+      {estaAberta && (
+        <ListaSuspensa>
+          {opcoes.map((opcao, index) => (
+            <ItemLista
+              onClick={() => setOpcaoSelecionada(opcao)}
+              focoAtivo={index === opcaoFocada}
+              key={opcao.value}
+            >
+              {opcao.text}
+            </ItemLista>
+          ))}
+        </ListaSuspensa>
+      )}
+    </LabelEstilizada>
+  );
+};
+
+export default ListaSupensa;
